daf01349a2b439c92240f6656327c8ebc883b909
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2021-08-24 14:41:38 +0700

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -186,7 +186,7 @@ class ConfusionMatrix:
             print(' '.join(map(str, self.matrix[i])))
 
 
-def bbox_iou(box1, box2, x1y1x2y2=True, GIoU=False, DIoU=False, CIoU=False, CDIoU=False, l_CDIoU=False, NCDIoU=False, NCDIoU_1=False, eps=1e-7):
+def bbox_iou(box1, box2, x1y1x2y2=True, GIoU=False, DIoU=False, CIoU=False, CDIoU=False, l_CDIoU=False, NCDIoU=False, NCDIoU_1=False, lco_CIoU=False, eps=1e-7):
     # Returns the IoU of box1 to box2. box1 is 4, box2 is nx4
     box2 = box2.T
 
@@ -199,9 +199,6 @@ def bbox_iou(box1, box2, x1y1x2y2=True, GIoU=False, DIoU=False, CIoU=False, CDIo
         b1_y1, b1_y2 = box1[1] - box1[3] / 2, box1[1] + box1[3] / 2
         b2_x1, b2_x2 = box2[0] - box2[2] / 2, box2[0] + box2[2] / 2
         b2_y1, b2_y2 = box2[1] - box2[3] / 2, box2[1] + box2[3] / 2
-        
-    box_1_new = torch.cat((b1_x1, b1_x2, b1_y1, b1_y2), 1)
-    box_2_new = torch.cat((b2_x1, b2_x2, b2_y1, b2_y2), 1)
         
     A = (b1_x1 - b2_x1)**2 + (b1_y1 - b2_y1)**2
     B = (b1_x2 - b2_x2)**2 + (b1_y2 - b2_y2)**2
@@ -220,13 +217,14 @@ def bbox_iou(box1, box2, x1y1x2y2=True, GIoU=False, DIoU=False, CIoU=False, CDIo
 
     iou = inter / union
     iou_true = (inter) / (union_true)
-    if GIoU or DIoU or CIoU or CDIoU or l_CDIoU or NCDIoU or l_NCDIoU:
+    if GIoU or DIoU or CIoU or CDIoU or l_CDIoU or NCDIoU or l_NCDIoU or lco_CIoU:
         cw = torch.max(b1_x2, b2_x2) - torch.min(b1_x1, b2_x1)  # convex (smallest enclosing box) width
         ch = torch.max(b1_y2, b2_y2) - torch.min(b1_y1, b2_y1)  # convex height
         if CIoU or DIoU or CDIoU or NCDIoU:  # Distance or Complete IoU https://arxiv.org/abs/1911.08287v1
             c2 = cw ** 2 + ch ** 2 + eps  # convex diagonal squared
             rho2 = ((b2_x1 + b2_x2 - b1_x1 - b1_x2) ** 2 + (b2_y1 + b2_y2 - b1_y1 - b1_y2) ** 2) / 4  # center distance squared
             diou = (A + B + C + D) / (4*c2)
+            lcd = (A + B) / (4*c2)
             
             if DIoU:
                 return iou - rho2 / c2  # DIoU
@@ -246,6 +244,10 @@ def bbox_iou(box1, box2, x1y1x2y2=True, GIoU=False, DIoU=False, CIoU=False, CDIo
                 with torch.no_grad():
                     alpha = v / (v - iou + (1 + eps))
                 return lo - (diou + v * alpha) 
+            elif lco_CIoU:
+                v = (4 / math.pi ** 2) * torch.pow(torch.atan(w2 / h2) - torch.atan(w1 / h1), 2)
+                lo = 0.8*iou + 0.2*iou_true
+                return 1 - v*lo + lcd 
                 
                 
         else:  # GIoU https://arxiv.org/pdf/1902.09630.pdf
